963a7c020ef409ba29079d783b7ee9f17c52ea98
jayant-dwivedi <jayant.dwivedi@squareboat.com>
2023-03-28 22:05:43 +0530

    fix: our work section

--- src/components/OurWork/index.tsx
+++ src/components/OurWork/index.tsx
@@ -14,7 +14,42 @@ const OurWork = () => {
       </div>
 
       <div className={styles.workImage}>
-        <img className={styles.starBucks} src="/images/startBucks.png" alt="" />
+        <div className={styles.nikeContainer}>
+          <img src="/images/startBucks.png" alt="" />
+          <p className={styles.marketing}>Digital Marketing</p>
+          <p className={styles.brandName}>StarBucks</p>
+          <p className={styles.descText}>
+            Our ad campaign brought 80% footfall to the company{" "}
+          </p>
+        </div>
+
+        <div className={styles.nikeContainer}>
+          <img className={styles.nikeImage} src="/images/nike.png" alt="nike" />
+          <p className={styles.marketing}>Digital Marketing</p>
+          <p className={styles.brandName}>Nike</p>
+          <p className={styles.descText}>
+            Our ad campaign brought 80% footfall to the company{" "}
+          </p>
+        </div>
+      </div>
+
+      <div className={styles.workImage}>
+        <div className={styles.nikeContainer}>
+          <img className={styles.nikeImage} src="/images/nike.png" alt="nike" />
+          <p className={styles.marketing}>Digital Marketing</p>
+          <p className={styles.brandName}>Nike</p>
+          <p className={styles.descText}>
+            Our ad campaign brought 80% footfall to the company{" "}
+          </p>
+        </div>
+        <div className={styles.nikeContainer}>
+          <img src="/images/startBucks.png" alt="" />
+          <p className={styles.marketing}>Digital Marketing</p>
+          <p className={styles.brandName}>StarBucks</p>
+          <p className={styles.descText}>
+            Our ad campaign brought 80% footfall to the company{" "}
+          </p>
+        </div>
       </div>
     </div>
   );
